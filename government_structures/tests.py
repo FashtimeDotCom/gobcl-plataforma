c5bf311a234ef4bf5d792d797073c14233e0467c
Cristian Sepulveda <cristian@magnet.cl>
2017-10-20 09:56:22 -0300

    delete TODO in all *_delete.pug

--- government_structures/tests.py
+++ government_structures/tests.py
@@ -4,6 +4,7 @@ from base.tests import BaseTestCase
 
 from .models import GovernmentStructure
 from regions.models import Region
+from public_servants.models import PublicServant
 
 
 class GovernmentStructureModelTest(BaseTestCase):
@@ -27,7 +28,19 @@ class GovernmentStructureModelTest(BaseTestCase):
     def test_duplicate_government_with_public_servants(self):
         tomorrow = self.now + timezone.timedelta(days=1)
         self.assertEqual(GovernmentStructure.objects.count(), 1)
+        self.assertEqual(PublicServant.objects.count(), 1)
         self.assertEqual(Region.objects.count(), 1)
         self.government_structure.duplicate(date=tomorrow)
         self.assertEqual(GovernmentStructure.objects.count(), 2)
         self.assertEqual(Region.objects.count(), 2)
+        self.assertEqual(PublicServant.objects.count(), 2)
+
+    def test_duplicate_government_withouth_public_servants(self):
+        tomorrow = self.now + timezone.timedelta(days=1)
+        self.assertEqual(GovernmentStructure.objects.count(), 1)
+        self.assertEqual(PublicServant.objects.count(), 1)
+        self.assertEqual(Region.objects.count(), 1)
+        self.government_structure.duplicate(date=tomorrow)
+        self.assertEqual(GovernmentStructure.objects.count(), 2)
+        self.assertEqual(Region.objects.count(), 2)
+        # self.assertEqual(PublicServant.objects.count(), 1)
