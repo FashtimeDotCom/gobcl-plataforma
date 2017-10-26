edfa66bed02fedb0f4a5465e262dbbf9cd5a06e8
Cristian <cristian@magnet.cl>
2017-10-26 15:18:04 -0300

    add comment in links.context_processors

--- links/context_processors.py
+++ links/context_processors.py
@@ -3,6 +3,11 @@ from .models import FooterLink
 
 
 def footer_links(request):
+    """
+    Includes the footer links by
+    structure government in the context
+    """
+
     context = {
         'footer_links': FooterLink.objects.by_government_structure(
             request.government_structure
