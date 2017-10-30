5be58ef96c8d1ba21f0a4db495a4b26034d2d334
Jorge Guerra <jfguerra00@gmail.com>
2017-10-30 17:15:48 -0300

    Add i18n urls

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 from base import views as base_views
 
@@ -31,6 +32,14 @@ urlpatterns = [
     url(r'^$', base_views.index, name='home'),
     url(r'^n/', include('cms.urls')),
 ]
+
+urlpatterns += i18n_patterns(
+    url(r'^accounts/', include('users.urls')),
+    url(r'^institutions/', include('institutions.urls')),
+    url(r'^ministries/', include('ministries.urls')),
+    url(r'^api/1.0/', include('api.urls')),
+    url(r'^$', base_views.index, name='home'),
+)
 
 if settings.DEBUG:
     import debug_toolbar
