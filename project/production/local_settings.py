163d980b48b00aff47475b0cbdb62c13ba23b1a3
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2017-11-03 16:03:16 -0300

    Add local settings to use within a docker container

---
+++ project/production/local_settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+LOCAL_DATABASES = {
+    'default': {
+        # engines: 'postgresql', 'mysql', 'sqlite3' or 'oracle'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'plataforma-gobcl',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+LOCALLY_INSTALLED_APPS = [
+]
+
+ENABLE_EMAILS = True
