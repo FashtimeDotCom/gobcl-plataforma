8550b4269c33c31abd7e8746a8796b8a9b8b6bd2
Jorge Guerra <jfguerra00@gmail.com>
2017-11-09 17:26:58 -0300

    Fix class name typo

--- regions/tests.py
+++ regions/tests.py
@@ -8,7 +8,7 @@ from django.utils.translation import activate
 from base.tests import BaseTestCase
 
 
-class RegioniTranslationTests(BaseTestCase):
+class RegionTranslationTests(BaseTestCase):
     def test_region_model_translation(self):
         """
         Tests that a region model is able to be translated
