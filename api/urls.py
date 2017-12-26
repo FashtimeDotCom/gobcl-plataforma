c6fe184e19aa43fbc76df4f61147bb176edfa1ea
Cristian <cristian@magnet.cl>
2017-12-26 15:30:02 -0300

    add search articles API

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,12 @@ from rest_framework.routers import DefaultRouter
 
 from regions.viewsets import CommuneViewSet
 from searches.viewsets import ArticleViewSet
+from searches.viewsets import ArticleSearchViewSet
 
 router = DefaultRouter()
 router.register(r'communes', CommuneViewSet, 'commune')
 router.register(r'articles', ArticleViewSet, 'article')
+router.register(r'search', ArticleSearchViewSet, 'article')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- searches/viewsets.py
+++ searches/viewsets.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
@@ -32,3 +34,26 @@ class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
             'app_config',
             'featured_image',
         )
+
+
+class ArticleSearchViewSet(ArticleViewSet):
+
+    def get_queryset(self):
+        queryset = super(ArticleSearchViewSet, self).get_queryset()
+
+        self.query = self.request.GET.get('q', '')
+        self.category_slug = self.request.GET.get('category_slug', '')
+
+        if self.query:
+            queryset = queryset.filter(
+                Q(translations__title__unaccent__icontains=self.query) |
+                Q(translations__lead_in__unaccent__icontains=self.query) |
+                Q(translations__search_data__unaccent__icontains=self.query)
+            ).distinct()
+
+        if self.category_slug:
+            queryset = queryset.filter(
+                categories__translations__slug__icontains=self.category_slug
+            ).distinct()
+
+        return queryset
