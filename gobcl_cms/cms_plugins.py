12c0406f5c78c2922525a1d39c10e0c2ceda60d0
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-12-26 12:37:02 -0300

    Fix gallery image edition

--- gobcl_cms/cms_plugins.py
+++ gobcl_cms/cms_plugins.py
@@ -1,14 +1,14 @@
 from django.utils.translation import ugettext_lazy as _
 
 from cms.plugin_base import CMSPluginBase
-from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_pool import plugin_pool
 
-from .models import ImagePlugin, HtmlPlugin
+from .models import GalleryImagePlugin, HtmlPlugin
+from .models import GalleryPlugin
 
 
 class GalleryCMSPlugin(CMSPluginBase):
-    model = CMSPlugin
+    model = GalleryPlugin
     cache = False
     name = _('Gallery')
     render_template = 'cms_plugins/gallery/gallery.pug'
@@ -18,14 +18,15 @@ class GalleryCMSPlugin(CMSPluginBase):
 
 class ImageCMSPlugin(CMSPluginBase):
     name = _('Image')
-    model = ImagePlugin
-    render_plugin = False
+    model = GalleryImagePlugin
+    render_template = 'cms_plugins/gallery/image.pug'
+    require_parent = True
+    parent_classes = ['GalleryCMSPlugin']
 
 
 class HtmlCMSPlugin(CMSPluginBase):
     name = _('HTML')
     model = HtmlPlugin
-    render_plugin = True
     render_template = 'cms_plugins/html.pug'
 
 
