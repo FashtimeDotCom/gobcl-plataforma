e0fd2ad73a430ede750bc3d30c917aac310c3971
Cristian <cristian@magnet.cl>
2017-12-26 14:57:55 -0300

    add paginate api articles by 'paginate by' from newsblogconfig

--- searches/viewsets.py
+++ searches/viewsets.py
@@ -4,21 +4,31 @@ from rest_framework.pagination import LimitOffsetPagination
 from .serializers import ArticleSerializer
 
 from aldryn_newsblog.models import Article
+from aldryn_newsblog.cms_appconfig import NewsBlogConfig
 
 
 class LimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 12
+
+    def get_limit(self, request):
+        newsblog_config = NewsBlogConfig.objects.filter(
+            namespace='aldryn_newsblog_default'
+        ).values('paginate_by')
+
+        default_limit = newsblog_config[0].get('paginate_by')
+
+        return default_limit
 
 
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     model = Article
     serializer_class = ArticleSerializer
     pagination_class = LimitOffsetPagination
-    # filter_fields = ('region',)
 
     def get_queryset(self):
-        return self.model.objects.filter().prefetch_related(
+        return self.model.objects.all().prefetch_related(
             'translations',
+            'featured_image',
         ).select_related(
             'app_config',
+            'featured_image',
         )
