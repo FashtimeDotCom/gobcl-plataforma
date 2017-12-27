6a1d2df5090c407e6a9339a6b5f5ee3dfaa6a3f8
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-12-27 17:43:59 -0300

    Use production compress url

--- project/staging/local_settings.py
+++ project/staging/local_settings.py
@@ -12,4 +12,5 @@ LOCALLY_ALLOWED_HOSTS = []
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
 COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
-COMPRESS_URL = 'https://s3.amazonaws.com/gobcl-staging/'
+# COMPRESS_URL = 'https://s3.amazonaws.com/gobcl-staging/'
+COMPRESS_URL = 'https://s3.amazonaws.com/gob.cl/'
