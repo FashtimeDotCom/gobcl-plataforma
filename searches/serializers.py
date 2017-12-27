bfd7cf94cd61b515a42cc1f8774c42f83a34a26b
Cristian <cristian@magnet.cl>
2017-12-27 11:13:50 -0300

    add alt_image and language

--- searches/serializers.py
+++ searches/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 from parler_rest.serializers import TranslatableModelSerializer
 from parler_rest.fields import TranslatedFieldsField
 
+from cms.utils.i18n import get_current_language
+from django.utils.translation import get_language
+
 from easy_thumbnails.templatetags.thumbnail import thumbnail_url
 
 from aldryn_newsblog.models import Article
@@ -19,7 +22,11 @@ class ArticleSerializer(TranslatableModelSerializer):
     url = serializers.ReadOnlyField(
         source='get_absolute_url',
     )
+    alt_image = serializers.ReadOnlyField(
+        source='featured_image.default_alt_text',
+    )
     featured_image = ThumbnailSerializer()
+    current_language = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
@@ -29,4 +36,10 @@ class ArticleSerializer(TranslatableModelSerializer):
             'featured_image',
             'publishing_date',
             'url',
+            'alt_image',
+            'current_language',
         )
+    
+    def get_current_language(self, obj):
+        language = get_language()
+        return language
