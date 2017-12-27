091e624098a0d68ccda97ee8b2991a11544de7d8
vnosikov <vasily@magnet.cl>
2017-12-27 12:24:21 -0300

    add infinite scroll condition  optimization

--- base/static/js/load_more_articles.js
+++ base/static/js/load_more_articles.js
@@ -7,14 +7,16 @@ $(function () {
     if (isAlreadySent) {
       return;
     }
-    // Fetch variables
-    var scrollTop = $(document).scrollTop();
-    var windowHeight = $(window).height();
-    var bodyHeight = $(document).height() - windowHeight;
-    var scrollPercentage = (scrollTop / bodyHeight);
 
-    // if the scroll is more than 90% from the top, load more content.
-    if(scrollPercentage > 0.9) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $('.loading-indicator').parent().offset().top;
+    var elemBottom = elemTop + $('.loading-indicator').height();
+
+    var shouldLoadMore = ((elemBottom <= docViewBottom * 0.95) && (elemTop >= docViewTop));
+
+    if(shouldLoadMore) {
       isAlreadySent = true;
       $('.loading-indicator').show();
 
