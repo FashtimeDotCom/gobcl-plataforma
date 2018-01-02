55a4f61b17a9c224db6d880a6b58bbd5a9ed7a88
Jorge Guerra <jfguerra00@gmail.com>
2018-01-02 04:15:05 -0300

    Add context processors cache

--- searches/context_processors.py
+++ searches/context_processors.py
@@ -1,12 +1,50 @@
+# standard
+import copy
+
+# django
+from django.core.cache import caches
+
+# models
 from aldryn_newsblog.models import Article
 
 
-def get_feature_news(request):
+def featured_news():
+    """
+    Returns a list of the 3 featured articles.
+    """
+    articles = Article.objects.filter(
+        is_featured=True
+    ).published().prefetch_related(
+        'categories'
+    )[:3]
 
-    feature_news = {
-        'feature_news': Article.objects.filter(
-            is_featured=True
-        ).published().prefetch_related('categories')[:3]
-    }
+    featured_news_list = []
 
-    return feature_news
+    for article in articles:
+        base_dict = article.__dict__
+        article_dict = copy.copy(base_dict)
+        article_dict['title'] = article.title
+        article_dict['categories'] = article.categories.all()
+        article_dict['get_absolute_url'] = article.get_absolute_url()
+        featured_news_list.append(article_dict)
+
+    return featured_news_list
+
+
+def get_featured_news(request):
+    """
+    Returns a dictionary with a list of featured news.
+    List is obtained by searching the cache.
+    If cache has no key for featured_news it will call
+    featured_news() and set it to the cache.
+    """
+    cache = caches['default']
+    featured_news_cache = cache.get('featured_news')
+    if not featured_news_cache:
+        featured_news_cache = cache.set(
+            'featured_news',
+            featured_news(),
+            86400,
+        )
+
+    return {'featured_news': featured_news_cache}
--- services/context_processors.py
+++ services/context_processors.py
@@ -1,10 +1,45 @@
+# standard
+import copy
+
+# django
+from django.core.cache import caches
+
+# models
 from .models import ChileAtiendeFile
 
 
+def chile_atiende_files(request):
+    """
+    Returns a list of 6 ChileAtiendeFile.
+    """
+
+    chile_atiende_files = ChileAtiendeFile.objects.all()[:6]
+    chile_atiende_files_list = []
+    for chile_atiende_file in chile_atiende_files:
+        base_dict = chile_atiende_file.__dict__
+        file_dict = copy.copy(base_dict)
+        file_dict['get_absolute_url'] = chile_atiende_file.get_absolute_url()
+        chile_atiende_files_list.append(file_dict)
+
+    return chile_atiende_files_list
+
+
 def get_chile_atiende_files(request):
+    """
+    Returns a dictionary with a list of ChileAtiendeFile.
+    List is obtained by searching the cache.
+    If cache has no key for chile_atiende_files it will call
+    chile_atiende_files() and set it to the cache.
+    """
+    cache = caches['default']
+    chile_atiende_files_cache = cache.get('chile_atiende_files')
+    if not chile_atiende_files_cache:
+        chile_atiende_files_cache = cache.set(
+            'chile_atiende_files',
+            chile_atiende_files(),
+            86400,
+        )
 
-    context = {
-        'chile_atiende_files': ChileAtiendeFile.objects.all()[:6]
+    return {
+        'chile_atiende_files': chile_atiende_files_cache
     }
-
-    return context
