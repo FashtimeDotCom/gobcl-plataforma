6b7430684fc6d4e7576865ffb21e4498c5ca7b1a
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2018-01-04 17:31:35 -0300

    Use local static files for deployment

--- project/production/local_settings.py
+++ project/production/local_settings.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
-
 import os
 
 DEBUG = False
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-LOCAL_DATABASES = {
-    'default': {
-        # engines: 'postgresql', 'mysql', 'sqlite3' or 'oracle'
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'plataforma-gobcl',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
-LOCALLY_INSTALLED_APPS = [
-]
+LOCALLY_INSTALLED_APPS = []
 
 ENABLE_EMAILS = True
 
@@ -30,12 +13,19 @@ if LOCALLY_ALLOWED_HOSTS:
     LOCALLY_ALLOWED_HOSTS = LOCALLY_ALLOWED_HOSTS.split(',')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
 THUMBNAIL_DEFAULT_STORAGE = DEFAULT_FILE_STORAGE
-COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
-COMPRESS_URL = 'https://s3-us-west-2.amazonaws.com/gob.cl/'
-MEDIA_URL = COMPRESS_URL
+MEDIA_URL = 'https://s3-us-west-2.amazonaws.com/gob.cl/'
 STATIC_URL = '/static/'
 
 CLAVE_UNICA_CALLBACK = 'https://gobcl.digital.gob.cl/callback'
 CLAVE_UNICA_CLIENT_ID = 'c242172b23d349018620dae9d39fd8ee'
+
+# s3 static flies
+COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
+COMPRESS_URL = MEDIA_URL
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
+
+# override with local static files
+COMPRESS_STORAGE = STATICFILES_STORAGE
+COMPRESS_URL = '/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
