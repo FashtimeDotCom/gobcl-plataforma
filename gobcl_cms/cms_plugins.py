278553bb03a4ddbd1b33a7fba8939ec15e30a0d0
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2018-01-08 08:09:36 -0300

    Add header and breadcrumb plugin

--- gobcl_cms/cms_plugins.py
+++ gobcl_cms/cms_plugins.py
@@ -1,10 +1,18 @@
 from django.utils.translation import ugettext_lazy as _
 
+from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
 from .models import GalleryImagePlugin, HtmlPlugin
 from .models import GalleryPlugin
+from .models import HeaderPlugin
+
+
+class BradcrumCMSPlugin(CMSPluginBase):
+    name = _('Breadcrumb')
+    model = CMSPlugin
+    render_template = 'cms_plugins/breadcrumb.pug'
 
 
 class GalleryCMSPlugin(CMSPluginBase):
@@ -30,6 +38,14 @@ class HtmlCMSPlugin(CMSPluginBase):
     render_template = 'cms_plugins/html.pug'
 
 
+class HeaderCMSPlugin(CMSPluginBase):
+    name = _('header')
+    model = HeaderPlugin
+    render_template = 'cms_plugins/header.pug'
+
+
+plugin_pool.register_plugin(BradcrumCMSPlugin)
 plugin_pool.register_plugin(GalleryCMSPlugin)
-plugin_pool.register_plugin(ImageCMSPlugin)
+plugin_pool.register_plugin(HeaderCMSPlugin)
 plugin_pool.register_plugin(HtmlCMSPlugin)
+plugin_pool.register_plugin(ImageCMSPlugin)
--- gobcl_cms/models.py
+++ gobcl_cms/models.py
@@ -2,6 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 from cms.models.pluginmodel import CMSPlugin
+from cms.models import Page
 
 from filer.fields.image import FilerImageField
 
@@ -39,3 +40,38 @@ class HtmlPlugin(CMSPlugin):
 
     def __str__(self):
         return self.html[:50]
+
+
+class HeaderPlugin(CMSPlugin):
+    title = models.CharField(
+        max_length=60,
+        verbose_name=_('title'),
+    )
+    description = models.TextField(
+        _('description'),
+        default='',
+        blank=True,
+    )
+    external_link = models.URLField(
+        verbose_name=_('External link'),
+        blank=True,
+        max_length=2040,
+        help_text=_('Provide a valid URL to an external website.'),
+    )
+    internal_link = models.ForeignKey(
+        Page,
+        verbose_name=_('Internal link'),
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        help_text=_('If provided, overrides the external link.'),
+    )
+    link_text = models.CharField(
+        max_length=60,
+        verbose_name=_('link text'),
+        blank=True,
+        default=''
+    )
+
+    def __str__(self):
+        return self.title
