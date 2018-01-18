9e275384be68f2d8f6593d887c5218ddc4b3b656
Cristian <cristian@magnet.cl>
2018-01-18 10:53:03 -0300

    fix error to get document.app

--- base/static/js/load_more_articles.js
+++ base/static/js/load_more_articles.js
@@ -3,31 +3,32 @@
 $(function () {
   if (document.app == undefined) {
     return
-  }
-  document.createInifiniteScroll(
-    // a link for ajax request
-    document.app.url + '?offset=' + parseInt(document.app.offset, 10),
-    // a function that adds information about category to a requestUrl
-    function (url) {
-      var currentLocation = window.location.href;
-      var match = currentLocation.match(/category\/([a-z-]+)\//);
-      var result = url;
-      if (match !== null) {
-        result += ('&category_slug=' + match[1]);
+  } else {
+    document.createInifiniteScroll(
+      // a link for ajax request
+      document.app.url + '?offset=' + parseInt(document.app.offset, 10),
+      // a function that adds information about category to a requestUrl
+      function (url) {
+        var currentLocation = window.location.href;
+        var match = currentLocation.match(/category\/([a-z-]+)\//);
+        var result = url;
+        if (match !== null) {
+          result += ('&category_slug=' + match[1]);
+        }
+        return result;
+      },
+      // a pug template which will be used to render results from server
+      'articles/article_miniature_list',
+      // a selector to an existing container
+      '.article-miniature-list',
+      // postLoadingTransformation
+      function () {
+        // Setting default images
+        $('img.default-image').each(function () {
+          $(this).attr('src', $('.article-miniature-list').
+            data('default-image-url'));
+        });
       }
-      return result;
-    },
-    // a pug template which will be used to render results from server
-    'articles/article_miniature_list',
-    // a selector to an existing container
-    '.article-miniature-list',
-    // postLoadingTransformation
-    function () {
-      // Setting default images
-      $('img.default-image').each(function () {
-        $(this).attr('src', $('.article-miniature-list').
-          data('default-image-url'));
-      });
-    }
-  )();
+    )();
+  }
 });
