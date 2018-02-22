3459f3668c6b6be4d83b04377656a441ccdaeadf
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2018-02-22 15:09:48 -0300

    Handle non 200 response on ChileAtiende API

--- searches/views.py
+++ searches/views.py
@@ -29,13 +29,17 @@ class ArticleListView(ListView):
 
         # obinta chile atiende files
         chile_atiende_file_client = File()
+
+        # set the list as empty by default
+        context['chile_atiende_files_json'] = []
         if self.request.GET.get('q'):
             if settings.CHILEATIENDE_ACCESS_TOKEN:
-                context['chile_atiende_files_json'] = json.loads(
-                    chile_atiende_file_client.list(query=self.query).text
-                )['fichas']['items']
-        else:
-            context['chile_atiende_files_json'] = []
+                response = chile_atiende_file_client.list(query=self.query)
+
+                if response.status_code == 200:
+                    context['chile_atiende_files_json'] = json.loads(
+                        response.text
+                    )['fichas']['items']
 
         # Count the total list of objects
         context['count'] = (
