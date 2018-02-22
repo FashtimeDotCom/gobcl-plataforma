71c733bb09161cf45aa33f8e339a766253ab3bd9
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2018-02-22 12:52:35 -0300

    Set AllowAny as default permission class on Django rest framework

--- project/settings.py
+++ project/settings.py
@@ -472,6 +472,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ]
