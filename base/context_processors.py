ec22f86235296fdeace9e92ea35ea96543685c54
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2018-03-12 19:26:47 -0300

    Refactor: only make one query on categories list

--- base/context_processors.py
+++ base/context_processors.py
@@ -7,9 +7,9 @@ def categories(request):
     Includes the list of categories in the context
     """
 
-    category_ids = list(set(Category.objects.order_by(
+    category_ids = Category.objects.order_by(
         'article__count__visits'
-    ).values_list('id', flat=True)))
+    ).values('id')
 
     context = {
         'categories': Category.objects.filter(id__in=category_ids)[:5]
