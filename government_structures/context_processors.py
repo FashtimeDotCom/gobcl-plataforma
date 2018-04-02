999355a5e3d848b8623cb70aa6828094bbfa3ced
Cristian <cristian@magnet.cl>
2018-04-02 16:13:48 -0300

    fix error when not found government structure

--- government_structures/context_processors.py
+++ government_structures/context_processors.py
@@ -1,8 +1,13 @@
 def add_government_structure_to_context(request):
     """ Includes the current government_structure in the context """
 
+    try:
+        government_structure = request.government_structure
+    except:
+        return {}
+
     context = {
-        'government_structure': request.government_structure,
+        'government_structure': government_structure,
     }
 
     return context
