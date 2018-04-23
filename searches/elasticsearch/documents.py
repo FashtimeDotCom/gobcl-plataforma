be4a9942a25c76f7399e56ab35c4c2f7b3a49d6c
Cristian <cristian@magnet.cl>
2018-04-23 16:38:58 -0300

    add index_page function

--- searches/elasticsearch/documents.py
+++ searches/elasticsearch/documents.py
@@ -21,6 +21,10 @@ from ministries.models import PublicService
 from presidencies.models import Presidency
 from articles.models import Article
 from public_servants.models import PublicServant
+from regions.models import Region
+from sociocultural_departments.models import SocioculturalDepartment
+from public_enterprises.models import PublicEnterprise
+from cms.models.pagemodel import Page
 
 from .search import ISearch, ISearchObj
 
@@ -79,6 +83,44 @@ class SearchIndex(DocType):
         ISearch(footer_links, cls).indexing()
 
     @classmethod
+    def index_region(cls):
+        regions = Region.objects.by_government_structure(
+            government_structure
+        )
+        ISearch(regions, cls).indexing()
+
+    @classmethod
+    def index_page(cls):
+        languages = ('es', 'en')
+        for language in languages:
+            activate(language)
+            pages = Page.objects.filter(
+                title_set__isnull=False,
+                is_home=False,
+                application_namespace__isnull=True,
+            )
+            for page in pages:
+                import ipdb
+                ipdb.set_trace()
+                print(page.title, page.pk)
+                search_index = ISearchObj(page, cls)
+                search_index.indexing()
+
+    @classmethod
+    def index_sociocultural_department(cls):
+        sociocultural_department = SocioculturalDepartment.objects.filter(
+            government_structure=government_structure
+        )
+        ISearch(sociocultural_department, cls).indexing()
+
+    @classmethod
+    def index_public_enterprise(cls):
+        public_enterprises = PublicEnterprise.objects.filter(
+            government_structure=government_structure
+        )
+        ISearch(public_enterprises, cls).indexing()
+
+    @classmethod
     def index_presidencies(cls):
         presidencies = Presidency.objects.filter(
             government_structure=government_structure
@@ -129,14 +171,22 @@ class SearchIndex(DocType):
     @classmethod
     def bulk_indexing(cls):
         cls.init()
-
+        # print('pages')
+        # cls.index_page()
+        print('public articles')
         cls.index_articles()
+
         activate('es')
+        print('sociocultural department')
+        cls.index_sociocultural_department()
+        print('public enterprises')
+        cls.index_public_enterprise()
+        print('regions')
+        cls.index_region()
         print('public servant')
         cls.index_public_servant()
         print('public presidency')
         cls.index_presidencies()
-        print('public articles')
         print('public public services')
         cls.index_public_services()
         print('campaigns')
