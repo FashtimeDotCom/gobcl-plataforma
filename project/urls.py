c518495e20d8a60f7dba09007345ae26f266bd74
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2018-05-14 15:35:38 -0400

    Rename articles url path to news

--- project/urls.py
+++ project/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
 ]
 
 urlpatterns += i18n_patterns(
-    url(_(r'^articles/'), include('articles.urls'), name='articles'),
+    url(_(r'^news/'), include('articles.urls'), name='articles'),
     url(_(r'^news/'), include('gobcl_cms.news_urls')),
     url(r'^api/1.0/', include('api.urls')),
     url(r'^admin/', include('loginas.urls')),
